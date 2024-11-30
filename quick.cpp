991304d4b620cd9507860550ff8eb3e55d5678a5
BruceLeesungho <havegood123@naver.com>
2024-11-30 19:27:10 +0900

    Update quick.cpp

--- quick.cpp
+++ quick.cpp
@@ -1,62 +1,52 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <stdlib.h>
+#define MAX (10000)
 
-void Quick_sort(int left, int right, int* arr);
-int N;
-int array_1[10000];
+void radixSort(int* arr, int n) {
+    int res[MAX];   // 결과 배열
+    int maxValue = 0;
+    int exp = 1;
 
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > maxValue) {
+            maxValue = arr[i];
+        }
+    }
 
-int main(void)
-{
-	scanf("%d", &N);
-	for (int i = 0; i < N; i++)
-	{
-		int n;
-		scanf("%d", &n);
-		array_1[i] = n;
-	}
-	Quick_sort(0, N - 1, array_1);
+    while (maxValue / exp > 0) 
+    {
+        int bucket[10] = { 0 };
+        for (int i = 0; i < n; i++) {
+            bucket[arr[i] / exp % 10]++;
+        }
 
+        for (int i = 1; i < 10; i++) {
+            bucket[i] += bucket[i - 1];
+        }
 
-	for (int i = 0; i < N; i++)
-	{
-		printf("%d\n", array_1[i]);
-	}
-	return 0;
+        for (int i = n - 1; i >= 0; i--) {
+            res[--bucket[a[i] / exp % 10]] = a[i];
+        }
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = res[i];
+        }
+
+        exp *= 10;
+    }
 }
 
+int main(void) {
+    int arr[MAX];
+    int i, n;
+    scanf("%d", &n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    radixSort(a, n);
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
 
-void Quick_sort(int l, int r, int* arr)
-{
-	if (l < 0 || r > N || l >= r) return;
-	
-	int mid = (l + r) / 2;
-	int left = l;
-	int right = r;
-
-	while (left <= right)
-	{
-		int temp;
-		while ((arr[left] < arr[mid]) && (left < N)) left++;
-		while ((arr[right] > arr[mid]) && (right > 0)) right--;
-		
-		if(left <= right)
-		{
-			temp = arr[left];
-			arr[left] = arr[right];
-			arr[right] = temp;  //swap
-
-			left++;
-			right--;
-		}
-	}
-	//int new_pivot = (left + pivot) / 2;
-
-	Quick_sort(l, right, arr);
-	Quick_sort(left, r, arr);
-
-
+    return 0;
 }
-
-

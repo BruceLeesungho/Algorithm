04759b0e5d66714f6beb811f88289496b2c536ca
BruceLeesungho <87701815+BruceLeesungho@users.noreply.github.com>
2025-04-05 21:41:27 +0900

    14501 DP

--- 14501.cpp
+++ 14501.cpp
@@ -1,41 +1,47 @@
-#define _CRT_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS_
 #include <iostream>
-#include <math.h>
-#include <queue>
+#include <stdlib.h>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
+int DP[15 + 2];
+int T[15 + 2];
+int P[15 + 2];
+
 int N;
-int T[15 + 1];
-int P[15 + 1];
-int DP[15 + 1];
-
-void make_dp(void)
-{
-	for (int i = N; i > 0; i--)
-	{
-		if (i + T[i] > N + 1) DP[i] = DP[i + 1]; //선택못함.
-		else
-		{
-			DP[i] = max(DP[i + 1], DP[T[i] + i] + P[i]);
-		}
-	}
-
-}
-
 
 int main(void)
 {
+	int maxmax = 0;
+
 	cin >> N;
 	for (int i = 1; i <= N; i++)
 	{
 		cin >> T[i] >> P[i];
 	}
-	DP[N + 1] = 0;
-	make_dp();
+	
+	DP[N+1] = 0;
+	
 
-	cout << DP[1];
+	for (int i = N; i > 0; i--)
+	{
+		int a = 0;
+		int b = 0;
+		a = DP[i + 1];
+
+		if (T[i] + i - 1 <= N)
+		{
+			b = DP[i + T[i]] + P[i];
+		}
+
+		int c = max(a, b);
+		DP[i] = c;
+		if (c > maxmax) maxmax = c;
+	}
+
+	cout << maxmax;
 
 	return 0;
 }
